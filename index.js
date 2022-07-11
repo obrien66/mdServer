a47a2b8ce14624733c5dbb4501eaa5e079ae4bc1
obrien66 <nicholas.obrien@ucc.on.ca>
2022-07-11 09:40:23 -0400

    404 doesn't work like 200. Interessant

--- index.js
+++ index.js
@@ -5,9 +5,31 @@ const colour = require("colour");
 const markdown = require("markdown").markdown;
 const mustache = require("mustache");
 // import settings
-const settings = require("./settings.json")
+const settings = require("./settings.json");
+
+var bookend = [settings.headfile, settings.footfile];
+var endTags = ["<html>", "</html>"]
+
+var buildSite = (tags, ends, md) => {
+	return tags[0] + ends[0] + md + tags[1] + ends[1]
+}
 
 var server = http.createServer((req, res) => {
+	let endFileData = [];
+	// iterate over the array and read each file
+	for (var i = 0; i < bookend.length; i++) {
+		// make a usable file path using settings and items from the array
+		let bookendFile = `./${settings.siteDir}/${bookend[i]}`;
+		fs.readFile(bookendFile, settings.encoding, (err, data) => {
+			if (err) {
+				throw err
+			}
+			else {
+				// read it and add that part to the array
+				endFileData.push(data);
+			}
+		});
+	}
 	// use the data we get from settings at the requested url to make a filename we can use
 	let file;
 	if (req.url[req.url.length - 1] === "/"){
@@ -21,21 +43,23 @@ var server = http.createServer((req, res) => {
 	}
 	// now we just grab the bugger and send him out into the world
 	fs.readFile(file, settings.encoding, (err, data) => {
+		// console.log(endTags);
+		// console.log(endFileData);
 		if (err) {
 			// what are you looking for????
 			res.writeHead(404, {"Content-Type": "text/html"});
-			res.end(`Sorry boss, ${file} machine broke`);
-			console.log(colour.red("404 " )+ file);
+			res.end(buildSite(endTags, endFileData, `Sorry boss, ${file} machine broke`));
+			console.log(colour.red("404 " ) + file);
 		}
 		else {
 			// I'm so proud of him for putting himself out there
 			res.writeHead(200, {"Content-Type": "text/html"});
-			res.end(markdown.toHTML(data));
-			console.log(colour.green("200 " )+ file);
+			res.end(buildSite(endTags, endFileData, markdown.toHTML(data)));
+			console.log(colour.green("200 " ) + file);
 		}
 	})
 })
-
+// no one knows what it means but it's provacative. GETS THE PEOPLE GOIN!
 server.listen(settings.port, () => {
 	console.log(`Server is running at ${settings.port}\n`);
 })
--- site/index.md
+++ site/index.md
@@ -1,3 +1,6 @@
 # Hello world 
 
 This is a markdown file that is being served on mdServer
+
+Does it work on the fly?
+yes
